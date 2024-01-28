31274a45cb65de6ad6f4eb28e637771506436861
Saúl Camacho <electrohead1200@gmail.com>
2024-01-28 10:58:36 -0600

    Configuración Axios, Login y Acciones del Login, Mensaje de error en la Uthenticación

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,8 @@
 import './LoginPage.css';
 import { useForm } from '../../hooks/useForm';
+import { useAuthStore } from '../../hooks';
+import { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 const loginFormFields = {
     loginEmail: '',
@@ -15,19 +18,28 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
+    const { starLogin, errorMessage } = useAuthStore();
+
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields)
 
     const{ registerName, registerEmail, registerPassword, registerPassword2, onInputChange:onRegisterInputChange} = useForm(registerFormFields)
     
     const loginSubmit = (event) => {
         event.preventDefault();
-        console.log({loginEmail, loginPassword})
+        starLogin({ email: loginEmail, password: loginPassword})
     }
 
     const registerSubmit = (event) => {
         event.preventDefault();
         console.log({registerName, registerEmail, registerPassword, registerPassword2})
     }
+
+    useEffect(() => {
+        if (errorMessage !== undefined) {
+          Swal.fire('Error en la autenticación', errorMessage, 'error')
+      }
+    }, [errorMessage])
+    
 
     return (
         <div className="container login-container">
